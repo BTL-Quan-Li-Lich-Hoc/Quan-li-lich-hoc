f956ee5e4ce45874c9bfa0f507220dfe013e92c9
cooc <minhdaonguyenad@gmail.com>
2026-09-16 11:01:17 +0700

    feat: add standalone UserDto contract

--- lib/core/contracts/models.dart
+++ lib/core/contracts/models.dart
@@ -1,9 +1,4 @@
-final class UserDto {
-  const new({required this.studentId, required this.displayName});
-
-  final String studentId;
-  final String displayName;
-}
+export 'user_dto.dart';
 
 final class SemesterDto {
   const new({
---
+++ lib/core/contracts/user_dto.dart
@@ -0,0 +1,6 @@
+final class UserDto {
+  const new({required this.studentId, required this.displayName});
+
+  final String studentId;
+  final String displayName;
+}
